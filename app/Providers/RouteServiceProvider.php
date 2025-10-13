b4b524748e62e170ba5dd03b49b27ff3714b25fa
Suhail Osman <35406282+sahlowle@users.noreply.github.com>
2025-10-13 22:38:53 +0300

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -82,7 +82,7 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::group(apply_filters('admin_route_group', [
                 'prefix' => 'admin',
-                'middleware' => ['web'],
+                'middleware' => ['web','website.locale'],
                 'namespace' => $this->namespace,
             ]), base_path('routes/web.php'));
 

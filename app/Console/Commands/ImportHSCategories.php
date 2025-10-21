22c3a9501d1b278daf8e8f8eda6fdbb82b1c9f32
Suhail Osman <35406282+sahlowle@users.noreply.github.com>
2025-10-21 12:44:07 +0300

    Update ImportHSCategories.php

--- app/Console/Commands/ImportHSCategories.php
+++ app/Console/Commands/ImportHSCategories.php
@@ -4,7 +4,9 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
+use Stichoza\GoogleTranslate\GoogleTranslate;
 
 class ImportHSCategories extends Command
 {
@@ -27,6 +29,22 @@ class ImportHSCategories extends Command
             return 1;
         }
 
+        // Disable foreign key checks
+        Schema::disableForeignKeyConstraints();
+
+        // Truncate the table
+        Category::truncate();
+
+        Category::create([
+            'id' => 1,
+            'name' => 'Uncategorized',
+            'slug' => 'uncategorized',
+            'parent_id' => null,
+            'order_by' => 1,
+            'is_searchable' => 1,
+            'is_featured' => 0,
+        ]);
+
         foreach ($data as $index => $item) {
             $enName = trim($item['en_name']);
             $arName = trim($item['ar_name']);
@@ -38,15 +56,24 @@ class ImportHSCategories extends Command
                 ['hs_code' => $hsCode],
                 [
                     'name' => [
-                        'en' => $enName,
-                        'ar' => $arName,
+                        'en' => $hsCode . ' - ' . $enName,
+                        'ar' => $hsCode . ' - ' . $arName,
+                        // 'be' => $hsCode . ' - ' . GoogleTranslate::trans($enName, 'be'),
+                        // 'bg' => $hsCode . ' - ' . GoogleTranslate::trans($enName, 'bg'),
+                        // 'bn' => $hsCode . ' - ' . GoogleTranslate::trans($enName, 'bn'),
+                        // 'ca' => $hsCode . ' - ' . GoogleTranslate::trans($enName, 'ca'),
+                        // 'et' => $hsCode . ' - ' . GoogleTranslate::trans($enName, 'et'),
+                        // 'fr' => $hsCode . ' - ' . GoogleTranslate::trans($enName, 'fr'),
+                        // 'ja' => $hsCode . ' - ' . GoogleTranslate::trans($enName, 'ja'),    
+                        // 'nl' => $hsCode . ' - ' . GoogleTranslate::trans($enName, 'nl'),
+                        // 'zh' => $hsCode . ' - ' . GoogleTranslate::trans($enName, 'zh'),
                     ],
                     'slug' => [
-                        'en' => Str::slug($enName),
-                        'ar' => Str::slug($arName, '-'),
+                        'en' => Str::slug($hsCode . ' - ' . $enName),
+                        'ar' => Str::slug($hsCode . ' - ' . $arName),
                     ],
                     'parent_id' => null,
-                    'order_by' => $index + 1,
+                    'order_by' => $index + 2,
                     'is_searchable' => true,
                     'is_featured' => false,
                     'product_counts' => 0,

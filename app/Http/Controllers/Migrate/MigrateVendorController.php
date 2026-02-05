d7b6d5806f3724ea2c505f1ef77a6a939af664c0
Suhail Osman <35406282+sahlowle@users.noreply.github.com>
2026-02-06 01:09:58 +0300

    uo

--- app/Http/Controllers/Migrate/MigrateVendorController.php
+++ app/Http/Controllers/Migrate/MigrateVendorController.php
@@ -13,10 +13,14 @@ class MigrateVendorController extends Controller
         $vendors = Vendor::query()
             ->with('shop')
             ->where('name', 'not like', '%zara%')
+            ->orWhere('email', 'not like', '%zara%')
+            ->orWhere('name', 'not like', '%test%')
+            ->orWhere('email', 'not like', '%test%')
             ->get();
 
         return response()->json([
             'status' => true,
+            'count' => $vendors->count(),
             'data' => VendorDetailResource::collection($vendors)
         ]);
     }
